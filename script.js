f586cc3e1749c4a355a98126d2967135888e4093
Risma227006144 <163633964+Risma227006144@users.noreply.github.com>
2024-04-05 23:14:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.menu-toggle').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.menu-toggle').addEventListener('click', function () {
         document.querySelector('nav h2').classList.toggle('active');
         document.querySelector('.menu-toggle').classList.toggle('active');
     });

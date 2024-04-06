8448d1a3ab2177cae1a0c58fb27c0fe8366e11e8
Risma227006144 <163633964+Risma227006144@users.noreply.github.com>
2024-04-06 21:57:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.menu-toggle').addEventListener('click', function () {
-        document.querySelector('nav h2').classList.toggle('active');
-        document.querySelector('.menu-toggle').classList.toggle('active');
-    });
-});

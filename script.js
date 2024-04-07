102728e96c9a7fb7d22ff4da8f3f7600fc0d02ff
Risma227006144 <163633964+Risma227006144@users.noreply.github.com>
2024-04-07 09:03:40 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navbarToggle = document.querySelector('.navbar-toggle');
-    const navbarLinks = document.querySelector('.navbar-links');
-
-    navbarToggle.addEventListener('click', function() {
-        navbarLinks.classList.toggle('active');
-    });
-});
